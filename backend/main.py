ee6fab80dabb8b8900c18661aff182d8ab2586f3
Gabriel Borela <gabrielborela07@gmail.com>
2025-07-23 18:28:51 -0300

    fix: Add Netlify domain to CORS allowed origins

--- backend/main.py
+++ backend/main.py
@@ -43,7 +43,8 @@ origins = [
     "http://localhost",
     "http://localhost:8000",
     "http://127.0.0.1:5500",
-    "http://localhost:5500"
+    "http://localhost:5500",
+    "https://starling-valkyrie-zaaff3.netlify.app" 
 ]
 
 app.add_middleware(
